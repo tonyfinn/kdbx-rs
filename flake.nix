9ea0e8b3ae34d4de477c10ce3290c7d5064cc2af
Tony Finn <git@tonyfinn.com>
2022-11-06 18:01:26 +0000

    Fix rust-analyzer + nix-direnv conflict

--- flake.nix
+++ flake.nix
@@ -1,28 +1,33 @@
 {
+
   description = "Keepass Library for Rust";
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      makeSystemMap = f: system: { name = system; value = f system; };
-      forAllSystems = f: builtins.listToAttrs (builtins.map (makeSystemMap f) systems);
-    in
-    {
-      packages = forAllSystems
-        (system:
-          let pkgs = nixpkgs.legacyPackages.${system};
-          in {
-            default = pkgs.rustPlatform.buildRustPackage
-              {
-                pname = "kdbx-rs";
-                version = "0.2.2";
-                src = ./.;
-                cargoLock = {
-                  lockFile = ./Cargo.lock;
-                };
-                nativeBuildInputs = [ pkgs.clippy ];
-              };
-          }
-        );
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "kdbx-rs";
+          version = "0.2.2";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            rustc
+            clippy
+          ];
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        };
+      }
+    );
 }
