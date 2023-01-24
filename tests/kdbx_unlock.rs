93c0ce920c2a28b5fc1ee7f53921357b93cc07ec
Samuel Tardieu <sam@rfc1149.net>
2023-01-24 10:14:54 +0100

    Add Argon2id tests

--- tests/kdbx_unlock.rs
+++ tests/kdbx_unlock.rs
@@ -15,3 +15,18 @@ fn kdbx4_argon2d() -> Result<(), kdbx_rs::Error> {
     let key = kdbx_rs::CompositeKey::from_password("kdbxrs");
     Ok(db.unlock(&key).map(|_| ())?)
 }
+
+#[test]
+fn kdbx4_argon2id() -> Result<(), kdbx_rs::Error> {
+    let mut file_path = PathBuf::new();
+    file_path.push(env!("CARGO_MANIFEST_DIR"));
+    file_path.push("res");
+    file_path.push("test_input");
+    file_path.push("kdbx4-argon2id.kdbx");
+
+    let file = fs::File::open(file_path).unwrap();
+
+    let db = kdbx_rs::from_reader(file).unwrap();
+    let key = kdbx_rs::CompositeKey::from_password("kdbxrs");
+    Ok(db.unlock(&key).map(|_| ())?)
+}
--- tests/round_trip.rs
+++ tests/round_trip.rs
@@ -1,3 +1,4 @@
+use kdbx_rs::binary::KdfParams;
 use kdbx_rs::database::{Entry, Field, Group};
 use kdbx_rs::{self, CompositeKey, Kdbx};
 
@@ -17,7 +18,7 @@ fn key() -> CompositeKey {
 }
 
 #[test]
-fn round_trip() -> Result<(), kdbx_rs::Error> {
+fn round_trip_default() -> Result<(), kdbx_rs::Error> {
     let mut db = kdbx_rs::Database::default();
     db.set_name(DATABASE_NAME.to_string());
     db.set_description(DATABASE_DESC.to_string());
@@ -32,6 +33,45 @@ fn round_trip() -> Result<(), kdbx_rs::Error> {
     group.add_entry(entry);
     db.replace_root(group);
     let mut kdbx = Kdbx::from_database(db);
+
+    let mut output_buf = Vec::new();
+    kdbx.set_key(key())?;
+    kdbx.write(&mut output_buf)?;
+
+    let reparsed = kdbx_rs::from_reader(&*output_buf)?;
+    let unlocked = reparsed.unlock(&key())?;
+    assert_eq!(unlocked.meta().database_name, DATABASE_NAME);
+    assert_eq!(unlocked.meta().database_description, DATABASE_DESC);
+    let root = unlocked.root();
+    assert_eq!(root.name(), GROUP_NAME);
+    let first_entry: &Entry = root.entries().collect::<Vec<_>>()[0];
+    assert_eq!(first_entry.title().unwrap(), ENTRY_NAME);
+    assert_eq!(first_entry.password().unwrap(), ENTRY_PASSWORD);
+    assert_eq!(root.times(), &group_times);
+    assert_eq!(first_entry.times(), &entry_times);
+
+    Ok(())
+}
+
+#[test]
+fn round_trip_argon2id() -> Result<(), kdbx_rs::Error> {
+    let mut db = kdbx_rs::Database::default();
+    db.set_name(DATABASE_NAME.to_string());
+    db.set_description(DATABASE_DESC.to_string());
+    let mut group = Group::default();
+    group.set_name(GROUP_NAME);
+    let group_times = group.times().clone();
+    let mut entry = Entry::default();
+    entry.add_field(Field::new("Title", ENTRY_NAME));
+    entry.set_password(ENTRY_PASSWORD);
+    entry.add_field(Field::new("Password", ENTRY_PASSWORD));
+    let entry_times = entry.times().clone();
+    group.add_entry(entry);
+    db.replace_root(group);
+    let mut kdbx = Kdbx::from_database(db);
+    if let KdfParams::Argon2 { variant, .. } = &mut kdbx.header_mut().kdf_params {
+        *variant = argon2::Variant::Argon2id;
+    }
 
     let mut output_buf = Vec::new();
     kdbx.set_key(key())?;
